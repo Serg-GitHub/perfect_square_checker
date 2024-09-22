1a798c85f6353afa28f13ed88412e9d862482747
[sergio eneide] <[sergio.eneide@protonmail.com]>
2024-09-22 20:21:04 +0100

    fifth commit

--- perfect_square_checker.c
+++ perfect_square_checker.c
@@ -7,9 +7,20 @@ int main()
     printf("Enter a positive integer: ");
     scanf("%d", &num);
 
-    // Declare variable sqrt_num and calculate it
-    int sqrt_num = (int)sqrt(num);  // Put result of sqrt_num into num for comparison
-    
+    /* Declare variable sqrt_num and calculate it
+    Put result of sqrt_num into num for comparison */
+    int sqrt_num = (int)sqrt(num); 
+
     // Check if square of sqrt_num is equal to user integer input
-    if (sqrt_num * sqrt_num == num);
+    if (sqrt_num * sqrt_num == num)
+        ;
+    {
+        printf("%d is a perfect square.\n", num);
+    }
+    else
+    {
+        printf("%d is not a perfect square.\n", num);
+    }
+
+    return 0;
 }
